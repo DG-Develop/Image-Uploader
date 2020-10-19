0c9abb39790f437ee2126da2c197305558987c61
DG-Develop <dgdband@gmail.com>
2020-10-18 23:17:11 -0500

    agregando eventos de drag and drop al proyecto

--- src/components/ImageCard.jsx
+++ src/components/ImageCard.jsx
@@ -1,24 +1,56 @@
-import React from "react";
+import React, { useRef } from "react";
 
 import './style/ImageCard.scss'
 import image from '../assets/static/image.svg'
 
-const ImageCard = () => (
-  <section className="ImageCard">
-    <div className="ImageCard__Header">
-      <h1 className="ImageCard__Header--title">Upload your image</h1>
-      <span className="ImageCard__Header--description">File should be Jpeg, Png,...</span>
-    </div>
-    <div className="ImageCard__Container"> 
-        <img className="ImageCard__Container--img" src={image} alt=""/>
-        <input className="ImageCard__Container--drop" type="file" name="myFile"/>
+const ImageCard = ({ onDrop }) => {
+  let buttonFile = useRef()
+  let dropRegion = useRef()
+
+  const handleClick = () => {
+    buttonFile.current.click()
+  }
+
+  const preventDefault = e => {
+    e.preventDefault()
+    e.stopPropagation()
+  }
+
+  const handleDrop = (e) => {
+    // Para que no se recargue y abra otra pestaña
+    preventDefault(e)
+    onDrop(e)
+  }
+
+  return (
+    <section className="ImageCard">
+      <div className="ImageCard__Header">
+        <h1 className="ImageCard__Header--title">Upload your image</h1>
+        <span className="ImageCard__Header--description">File should be Jpeg, Png,...</span>
+      </div>
+      <div
+        className="ImageCard__Container"
+        onClick={handleClick}
+        onDragEnter={preventDefault}
+        onDragLeave={preventDefault}
+        onDragOver={preventDefault}
+        onDrop={handleDrop}
+        ref={dropRegion}>
+        <img className="ImageCard__Container--img" src={image} alt="" />
+        <input
+          className="ImageCard__Container--drop"
+          type="file"
+          ref={buttonFile}
+          name="myFile"
+          accept="image/png, image/jpeg" />
         <span className="ImageCard__Container--description">Drag & Drop your image here</span>
-    </div>
-    <div className="ImageCard__Footer">
+      </div>
+      <div className="ImageCard__Footer">
         <span className="ImageCard__Footer--text">Or</span>
-        <button className="ImageCard__Footer--btn">Choose a file</button>
-    </div>
-  </section>
-);
+        <button className="ImageCard__Footer--btn" onClick={handleClick}>Choose a file</button>
+      </div>
+    </section>
+  );
+}
 
 export default ImageCard;
--- src/pages/ImagePage.js
+++ src/pages/ImagePage.js
@@ -4,13 +4,25 @@ import SuccessCard from '../components/SuccessCard';
 import Uploading from '../components/Uploading';
 
 class ImagePage extends Component {
+
+  handleDrop = e => {
+    const data = e.dataTransfer
+    const file = data.files
+
+    this.handleFiles(file)
+  }
+
+  handleFiles = file => {
+    /* for (var i = 0, len = files.length; i < len; i++) {
+        if (validateImage(files[i]))
+            previewAnduploadImage(files[i]);
+    } */
+    console.log(file)
+  }
+
   render() {
     return (
-      <Fragment>
-        <ImageCard />
-        <Uploading />
-        <SuccessCard />
-      </Fragment>
+        <ImageCard onDrop={this.handleDrop}/>
     );
   }
 }
